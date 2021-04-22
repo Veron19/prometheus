22aa86a6e11d75ded565c182f59abefd214f85da
Veron19 <kravchenkoveron@gmail.com>
2021-04-22 12:28:19 +0300

    counter

---
+++ 5_2_counter.py
@@ -0,0 +1,37 @@
+import sys
+import math
+import collections
+#numpy.array_equal(a, b)
+a = (sys.argv[1])
+#b = (sys.argv[2])
+#for i in a:
+#    a == b
+#if i in b:
+#    b.counter (i)
+#    print('b.counter= '+str(b.counter))
+#else: print("Lists a and b are not equal")
+len (a)
+#len (b)
+#
+#print ('d= '+str(d))
+print (len(a))
+#print (len (b))
+#c = collections.Counter()
+#d =[]
+#for i in a:
+#    if i in b:
+d=collections.Counter()
+for num in ['123', '12345', '123', 'counter', '12', '12', '12']:
+    d[num] += 1
+    print ('d= '+str(d))
+#    else print ("Hello")
+#if collections.Counter(a) == collections.Counter(b):
+#     print ("The lists a and b are the same")
+#else: print ("The lists a and b are not the same")
+
+#for word in ['spam', 'egg', 'spam', 'counter', 'counter', 'counter']:
+#    c[word] += 1
+
+#    print(c)
+
+#    print(c['counter'])
